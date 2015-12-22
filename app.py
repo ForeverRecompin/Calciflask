926f036fddaa049d9b17c80147ae2d4de3404d84
A Jain <foreverrecompin@gmail.com>
2015-12-23 05:17:02 +0530

    Got rid of templates for each operation for a single one

--- app.py
+++ app.py
@@ -8,32 +8,32 @@ bootstrap = Bootstrap(app)
 @app.route('/add/<float:num1>/<int:num2>')
 @app.route('/add/<float:num1>/<float:num2>')
 def add(num1, num2):
-    name = 'Calciflask'
-    return render_template('add.html', name=name, num1=num1, num2=num2)
+    context = {'name':'Calciflask', 'title_desc': 'Addition', 'num1':num1, 'num2':num2, 'result':(num1 + num2), 'operator':'+'}
+    return render_template('operation.html', **context)
 
 @app.route('/sub/<int:num1>/<int:num2>')
 @app.route('/sub/<int:num1>/<float:num2>')
 @app.route('/sub/<float:num1>/<int:num2>')
 @app.route('/sub/<float:num1>/<float:num2>')
 def sub(num1, num2):
-    name = 'Calciflask'
-    return render_template('sub.html', name=name, num1=num1, num2=num2)
+    context = {'name':'Calciflask', 'title_desc': 'Subtraction', 'num1':num1, 'num2':num2, 'result':(num1 - num2), 'operator':'-'}
+    return render_template('operation.html', **context)
 
 @app.route('/mult/<int:num1>/<int:num2>')
 @app.route('/mult/<int:num1>/<float:num2>')
 @app.route('/mult/<float:num1>/<int:num2>')
 @app.route('/mult/<float:num1>/<float:num2>')
 def mult(num1, num2):
-    name = 'Calciflask'
-    return render_template('mult.html', name=name, num1=num1, num2=num2)
+    context = {'name':'Calciflask', 'title_desc': 'Multiplication', 'num1':num1, 'num2':num2, 'result':(num1 * num2), 'operator':'x'}
+    return render_template('operation.html', **context)
 
 @app.route('/div/<int:num1>/<int:num2>')
 @app.route('/div/<int:num1>/<float:num2>')
 @app.route('/div/<float:num1>/<int:num2>')
 @app.route('/div/<float:num1>/<float:num2>')
 def div(num1, num2):
-    name = 'Calciflask'
-    return render_template('div.html', name=name, num1=num1, num2=num2)
+    context = {'name':'Calciflask', 'title_desc': 'Division', 'num1':num1, 'num2':num2, 'result':(num1 / num2), 'operator':'/'}
+    return render_template('operation.html', **context)
 
 @app.route('/')
 def index():
